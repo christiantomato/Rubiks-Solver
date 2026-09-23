a9bb687c24ca552b86dae45852eb947dc9d51fad
christiantomato <christianntamayo@gmail.com>
2026-09-23 00:31:45 -0400

    rework indexing convention

--- cube_representation/cube2x2.py
+++ cube_representation/cube2x2.py
@@ -9,21 +9,30 @@ class Cube2x2:
     def __init__(self):
         """Initalizes a solved 2x2 cube. 
 
-        The convention fixes the UFR corner with white top and green front. The corners are given an index
-        starting there from 0, clockwise along the F face, and then clockwise again on the B side. Corners are 
-        also represented in a 3D coordinate system where the reference corner is in the 1st of the 8 quadrants.
+        The convention fixes the UFR corner with white top and green front. 
+        Every other corner is then given an index starting from DFR = 0, 
+        clockwise along the F face, and then clockwise again on the B side. 
+        Corners are also represented in a 3D coordinate system where the 
+        reference corner is in the 1st of the 8 quadrants.
 
-        UFR = 0
-        DFR = 1
-        DFL = 2
-        UFL = 3
-        UBR = 4
-        DBR = 5
-        DBL = 6
-        UBL = 7
+        DFR = 0
+        DFL = 1
+        UFL = 2
+        UBR = 3
+        DBR = 4
+        DBL = 5
+        UBL = 6
 
         The orientations are encoded as a number belonging to mod 3 where 0 represents correctly oriented.
         """
 
-        self.permutations = [1, 2, 3, 4, 5, 6, 7]
-        self.orientations = [0, 0, 0, 0, 0, 0, 0]
+        #initalize the solve state
+        self.permutations = [0, 1, 2, 3, 4, 5, 6] #each corner in its location
+        self.orientations = [0, 0, 0, 0, 0, 0, 0] #each corner oriented
+
+    def apply_move(self, move):
+            """Applies a single generator move to the cube state.
+
+            move: string identifier for the move (e.g. "U")
+            """
+
